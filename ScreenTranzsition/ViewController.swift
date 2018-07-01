1eaa7670b83fa78e9cd0110c526ac5f805a33b89
yamasaaki <myamasaaki@gmail.com>
2018-07-01 09:40:28 +0900

    Initial Commit

---
+++ ScreenTranzsition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenTranzsition
+//
+//  Created by Yamada Masaaki on 2018/07/01.
+//  Copyright © 2018年 Yamada Masaaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
