0c6cd5e9e0a147861140ba4e3f32d7dc8788128a
yamasaaki <myamasaaki@gmail.com>
2018-07-01 10:19:22 +0900

    新規追加

---
+++ ScreenTranzsition/SecoundViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecoundViewController.swift
+//  ScreenTranzsition
+//
+//  Created by Yamada Masaaki on 2018/07/01.
+//  Copyright © 2018年 Yamada Masaaki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecoundViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //test
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
